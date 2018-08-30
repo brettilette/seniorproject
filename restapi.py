2008ca6bd3568a300b993546fbddd766583c6f4e
rtjames <jamest699@gmail.com>
2018-08-30 14:25:23 -0400

    test

--- restapi.py
+++ restapi.py
@@ -16,8 +16,15 @@ class CompanyInfo(Resource):
         #result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(firstName = 'Jimmothy',lastName = 'Simmons',companyName = 'none')
 
+class CompanyInfo_Name(Resource):
+    def get(self,company_name):
+        #conn = db_connect.connect()
+        #query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        #result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return jsonify(firstName = 'Jimmothy',lastName = 'Simmons',companyName = company_name)
 
 api.add_resource(CompanyInfo, '/companyinfo')
+api.add_resource(CompanyInfo_Name, '/companyinfo/<company_name>')
 
 
 if __name__ == '__main__':
