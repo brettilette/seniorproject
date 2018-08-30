dd84c7372c66fe09e3524bd0f4ab26f1b36da86a
rtjames <jamest699@gmail.com>
2018-08-30 14:04:07 -0400

    test

--- restapi.py
+++ restapi.py
@@ -1,30 +1,25 @@
-from flask import Flask
-from flask import Response
-from flask import json
-from flask import jsonify
-from flask import request
-from flask import Blueprint
-from flask import render_template
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
 
+#db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/_api/hydraGraph')
-def api_response():
-    db = GraphDatabase("http://localhost:7475/db/data")
 
-    nodes = getNodes(db)
-    rels = getRels(db)
+class CompanyInfo(Resource):
+    def get(self):
+        #conn = db_connect.connect()
+        #query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        #result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        result = ('First Name:','Jimothy','Last Name:','Simmons')
+        return jsonify(result)
 
-    q = "START n = node(*) RETURN n"
-    params = {}
-    querySquenceObject = db.query(q, params = params, returns = RAW)
 
-    for node in querySquenceObject[1:]:
-        n = node.pop()
-        data = n.get('data')
-        name = data.get('name')
-        description = data.get('description')
+api.add_resource(CompanyInfo, '/companyinfo')
 
-        self = n.get('self')
-        self = urlparse(self)
-        uid = doRegEX(self)
+
+if __name__ == '__main__':
+    app.run(port='5002')
