fa3531fe748d5dd856f898e0f5601420d439eb2c
rtjames <jamest699@gmail.com>
2018-08-30 16:17:22 -0400

    added project name

--- restapi.py
+++ restapi.py
@@ -15,15 +15,15 @@ class Config(object):                       # Config for FLASK
 app.config.from_object(Config)
 
 class CompanyInfo(Resource):
-    def get(self, company_name):
+    def get(self, project_name,company_name):
         # create neo4j connection variable
         # pass company_name to the web scraper
         # result = store web scraper info
         # store result in neo4j
-        return jsonify(firstName = 'Jimmothy',lastName = 'Simmons',companyName = company_name)      # replace w/result
+        return jsonify(firstName = 'Jimmothy',lastName = 'Simmons',companyName = company_name,projectName = project_name)      # replace w/result
 
 
-api.add_resource(CompanyInfo, '/companyinfo/<company_name>')
+api.add_resource(CompanyInfo, '/companyinfo/<project_name>/<company_name>')
 
 
 if __name__ == '__main__':  # run api on 127.0.0.1:5002
