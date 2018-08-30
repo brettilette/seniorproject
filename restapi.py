2b4f69d5eae37530899f3872ea918b609766a109
rtjames <jamest699@gmail.com>
2018-08-30 14:05:31 -0400

    test

--- restapi.py
+++ restapi.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
+#from sqlalchemy import create_engine
+#from json import dumps
 from flask.ext.jsonpify import jsonify
 
 #db_connect = create_engine('sqlite:///chinook.db')
