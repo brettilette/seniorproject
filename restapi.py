862c39652dc886edfb81a3efe23af80cc31870e1
rtjames <jamest699@gmail.com>
2018-09-02 19:44:53 -0400

    added temp db

--- restapi.py
+++ restapi.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-# import neo4j
+from neo4j.v1 import GraphDatabase, basic_auth
+
+
+driver = GraphDatabase.driver("bolt://hobby-egcmkjgcconogbkekpcmcobl.dbs.graphenedb.com:24786", auth=basic_auth("csa-laptop", "b.xAEJfrSq4KEn.tTSqgx0mSYCQq2Vx"))
 
 
 # Connect to neo4j
@@ -14,16 +17,19 @@ class Config(object):                       # Config for FLASK
 
 app.config.from_object(Config)
 
-class CompanyInfo(Resource):
+class GetLinkedInEmployees(Resource):
     def get(self, project_name,company_name):
-        # create neo4j connection variable
-        # pass company_name to the web scraper
-        # result = store web scraper info
-        # store result in neo4j
-        return jsonify(firstName = 'Jimmothy',lastName = 'Simmons',companyName = company_name,projectName = project_name)      # replace w/result
+        session = driver.session()
+        cypher = "MERGE ( %s:Tag { name: %s } )\n" % (project_name, project_name)
+        cypher += "ON CREATE SET %s.created = timestamp() + 'LinkedIn'\n" % (project_name)
+        cypher += "ON MATCH SET %s.LinkedInModded = timestamp()\n" % (project_name)
+        cypher += "RETURN %s.name, %s.created, %s.LinkedInModded" % (project_name)
+        result = session.run(cypher)
+        session.close()
+        return jsonify(result)      # replace w/result
 
 
-api.add_resource(CompanyInfo, '/companyinfo/<project_name>/<company_name>')
+api.add_resource(GetLinkedInEmployees, '/get/linkedin/employees/<project_name>/<company_name>')
 
 
 if __name__ == '__main__':  # run api on 127.0.0.1:5002
