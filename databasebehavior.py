cc9568bafaa82253a39c86a731ddab561f12ba34
rtjames <jamest699@gmail.com>
2018-09-05 17:42:17 -0400

    Added Multithreading

--- databasebehavior.py
+++ databasebehavior.py
@@ -8,25 +8,6 @@ from workerbehavior import  find_node_type, do_work
 logging.basicConfig(filename='databasebehaivor.log',level=logging.DEBUG)
 numberOfThreads = 3
 
-# cypher = "Match (c)-[:HAS_TAG]->(t)\nWHERE c:Company AND t:Tag AND size((c)-[:]-()) = 1\nRETURN t.name, c.name"
-#
-# results = session.run(cypher)
-# json = requests.get("127.0.0.1/get/linkedin/employees/%s/%s" % (results[0],results[1]))
-#
-# query = """WITH {json} as data
-# UNWIND data.items as item
-# MERGE (p:Person {firstname: item.firstname, lastname: item.lastname})
-# ON CREATE SET p.created = timestamp() + ' by LinkedIn'
-# ON MATCH SET p.LastSeenByLinkedIn = timestamp()
-# MERGE (la:LinkedInAccount {address: item.LinkedInAccount})
-# ON CREATE SET la.created = timestamp() + ' by LinkedIn'
-# ON MATCH SET la.LastSeenByLinkedIn = timestamp()
-# MERGE (p)-[:HAS_ACCOUNT]->(la)"""
-#
-# session.run(query,json=json)
-#
-# session.close()
-
 
 class Worker(Thread):
 
