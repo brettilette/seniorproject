30a321eeb5ba3be8c563b80bc87c97ea609897e8
brettillette <42754117+brettillette@users.noreply.github.com>
2018-10-07 13:30:54 -0400

    tested workHistory, added changes

--- workHistory.py
+++ workHistory.py
@@ -4,16 +4,20 @@ import statistics
 #returns an array with the average at 0 and the stdDev at 1
 def workHistoryAnalyser(workHistory):
     dateProcessing = None
-    startDateParse = None
-    endDateParse = None
+    startDateParse = []
+    endDateParse = []
     tempArray = []
     days = []
     returnArray = []
     
     for x in workHistory:
         dateProcessing = x.split('-')
-        startDateParse = dateProcessing[0].split(' ')
-        endDateParse = dateProcessing[1].split(' ')
+        dateParse = dateProcessing[0].split(' ')
+
+        startDateParse.append(dateParse[0])
+        startDateParse.append(dateParse[1])
+        endDateParse.append(dateParse[3])
+        endDateParse.append(dateParse[4])
 
         tempArray = dateParser(startDateParse)
         startDate = date(tempArray[0], tempArray[1], tempArray[2])
@@ -23,6 +27,9 @@ def workHistoryAnalyser(workHistory):
         endDate = date(tempArray[0], tempArray[1], tempArray[2])
 
         days.append(abs(endDate-startDate).days)
+        
+        startDateParse = []
+        endDateParse = []
     
     average = sum(days)/len(days)
     stdDev = statistics.stdev(days)
@@ -35,30 +42,33 @@ def dateParser(date):
     #Year, Month, Day
     returnArray = [0,0,1]
     if date[0] == 'Jan':
-        returnArray[2] = 1
+        returnArray[1] = 1
     elif date[0] == 'Feb':
-        returnArray[2] = 2 
+        returnArray[1] = 2 
     elif date[0] == 'Mar':
-        returnArray[2] = 3 
+        returnArray[1] = 3 
     elif date[0] == 'Apr':
-        returnArray[2] = 4 
+        returnArray[1] = 4 
     elif date[0] == 'May':
-        returnArray[2] = 5 
+        returnArray[1] = 5 
     elif date[0] == 'Jun':
-        returnArray[2] = 6 
+        returnArray[1] = 6 
     elif date[0] == 'Jul':
-        returnArray[2] = 7 
+        returnArray[1] = 7 
     elif date[0] == 'Aug':
-        returnArray[2] = 8 
+        returnArray[1] = 8 
     elif date[0] == 'Sept' or date[0] == 'Sep':
-        returnArray[2] = 9 
+        returnArray[1] = 9 
     elif date[0] == 'Oct':
-        returnArray[2] = 10 
+        returnArray[1] = 10 
     elif date[0] == 'Nov':
-        returnArray[2] = 11 
+        returnArray[1] = 11 
     elif date[0] == 'Dec':
-        returnArray[2] = 12
+        returnArray[1] = 12
 
-    returnArray[0] == date[1] 
+    returnArray[0] = int(date[1]) 
     return returnArray
 
+#testData = ["Oct 2014 – Jun 2018", "Jun 2018 – Aug 2018"]
+
+#workHistoryAnalyser(testData)
