d5970d44495ecce3ae2be578663a3bd24be1dd95
Chandler Brown <ChandlerFBrown@gmail.com>
2018-11-02 15:02:34 -0400

    LinkedIn.py fixes and added dates_employed field

--- LinkedIn.py
+++ LinkedIn.py
@@ -5,6 +5,8 @@ Created on Tue Aug 28 13:03:23 2018
 @author: Chand
 """
 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from random import randint
 from time import sleep
@@ -13,8 +15,7 @@ import requests
 import bs4
 from secrets import *
 
-company_name = "Cybriant"
-num_pages = 5
+num_pages = 1 # Hard Coded Number of Pages - This number can be changed in the future based off company size (1 page ~ 10 people)
 
 def google_profile_pull(company_name, num_pages):
     '''
@@ -24,7 +25,7 @@ def google_profile_pull(company_name, num_pages):
     '''
     url_list = []
     print("Now searching Google for " + company_name + " employees...")
-    for i in range(0,num_pages-1):
+    for i in range(0,num_pages):
         sleep(randint(0,5))
         req = requests.get(r'https://www.google.com/search?q=site:linkedin.com/in/ "' + company_name + '"&start=' + str(i) + '0')
         soup = bs4.BeautifulSoup(req.text, 'lxml')
@@ -39,52 +40,97 @@ def google_profile_pull(company_name, num_pages):
 
 def linkedin_sign_in(browser):
     '''
-    Initializes browser by logging into fake profile Jimothy Simmons.
+    Initializes linkedin browser by logging into fake profile Jimothy Simmons.
     '''
+    
     browser.get("https://www.linkedin.com/uas/login?_l=en")
-    sleep(randint(0,5))
-    browser.find_element_by_id("session_key-login").send_keys(LINKEDIN_LOGON)
-    sleep(randint(0,5))
-    browser.find_element_by_id("session_password-login").send_keys(LINKEDIN_PASS)
-    browser.find_element_by_id("btn-primary").click()
+    sleep(randint(2,5))
+    
+    # New UI Login
+    try:
+        browser.find_element_by_id("username").send_keys(LINKEDIN_LOGON)
+        browser.find_element_by_id("password").send_keys(LINKEDIN_PASS)
+        browser.find_element_by_id("password").send_keys(Keys.RETURN)
+        sleep(randint(0,5))
+        
+    # Old UI Login
+    except:
+        browser.find_element_by_id("session_key-login").send_keys(LINKEDIN_LOGON)
+        browser.find_element_by_id("session_password-login").send_keys(LINKEDIN_PASS)
+        browser.find_element_by_id("session_password-login").send_keys(Keys.RETURN)
+        sleep(randint(0,5))
+        
     
 def pull_data(browser, url_list, company_name):
     '''
     Iterates over the url_list and pulls a defined set of information from the
     web pages: name (first and last) and company.
     '''
+    
     linkedin_sign_in(browser)
     main = []
+    
     for i, url in enumerate(url_list):
         job_length = []
+        dates_employed = []
+        
         try:
             browser.get(url)
             sleep(randint(5,10))
             HTML = browser.page_source
             soup = bs4.BeautifulSoup(HTML, 'lxml')
-            name = soup.select('h1[class="pv-top-card-section__name inline Sans-26px-black-85%"]')[0].get_text().strip()
+            
+            name = soup.select('h1[class="pv-top-card-section__name inline t-24 t-black t-normal"]')[0].get_text().strip() # Selector Subject to Change
             fname, lname = name.split(" ")           
-            company = soup.select('span[class="pv-top-card-v2-section__entity-name pv-top-card-v2-section__company-name text-align-left ml2 Sans-15px-black-85%-semibold lt-line-clamp lt-line-clamp--multi-line ember-view"]')[0].get_text().strip()
-            print(str(i) + ". " + fname + " " + lname + " " + company)
+            company = soup.select('button[class="pv-top-card-v2-section__link pv-top-card-v2-section__link-experience mb1"]')[0].get_text().strip() # Selector Subject to Change
+            print(str(i+1) + ". " + fname + " " + lname + " - " + company)
+            
             for i in range(5):
                 try:
                     job_length.append(str(soup.select('span[class="pv-entity__bullet-item-v2"]')[i].get_text()))
                 except:
-                    print(fname + " had less than 5 past experiences.")
-                    break
+                    pass
+                try:
+                    date = str(soup.select('h4[class="pv-entity__date-range t-14 t-black t-normal"]')[i].get_text())
+                except:
+                    try:
+                        date = str(soup.select('h4[class="pv-entity__date-range t-14 t-black--light t-normal"]')[i].get_text())
+                    except:
+                        pass
+                    pass
+                date = date.replace('Dates Employed','')
+                date = date.strip('\n')
+                dates_employed.append(date.replace('Dates Employed',''))
             print(job_length)
-            main.append([url, fname, lname, company, company_name, job_length])
+            print(dates_employed)
+            main.append([url, fname, lname, company, company_name, dates_employed, job_length])
+            
         except:
             print(str(i) + ". Mission failed. We'll get em next time: ")
-            print(str(url))
+            print("\t" + str(url))
 
-    return pd.DataFrame(main, columns = ['URL', 'FirstName', 'LastName', 'Company', 'Searched Term', 'JobLength'])
+    return pd.DataFrame(main, columns = ['URL', 'FirstName', 'LastName', 'Company', 'Searched Term', 'Dayes Employed', 'Job Length'])
 
 def FindEmployees(company):
     url_list = google_profile_pull(company, num_pages)
+    
+    # Final Version Should Run Headless
+# =============================================================================
+#     options = Options()
+#     options.headless = True
+#     driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+#     
+# =============================================================================
     browser = webdriver.Chrome(CHROMEDRIVER_PATH)
-    data = pull_data(browser, url_list, company_name)
-    return data.to_json(orient='records')
+    data = pull_data(browser, url_list, company)
+    return data
+    browser.close()
+    # return data.to_json(orient='records') 
+
+
+
+
+
 
 # =============================================================================
 # GET ALL LINKS FROM URL (NON JS)
