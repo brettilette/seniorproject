b87453f22ebaec1be01fe282336205733a70149e
jluttrell117 <42498094+jluttrell117@users.noreply.github.com>
2018-11-16 16:35:55 -0500

    User Login/Registration implemented

--- webstuff/app/routes.py
+++ webstuff/app/routes.py
@@ -1,15 +1,44 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect(url_for('home'))
-    return render_template('login.html', title='Login', form=form)
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('home')
+		return redirect(next_page)
+	return render_template('login.html', title='Login', form=form)
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('login'))
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('login'))
+	return render_template('register.html', title="Registration", form=form)
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
 	return render_template('home.html', title = 'Home')
 @app.route('/createProject', methods=['GET','POST'])
